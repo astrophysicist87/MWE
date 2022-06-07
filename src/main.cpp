1f1add3f29ab1d6f92469b98e85bd653c4c1e432
Christopher Plumberg <plumberg@jnorhos-03.physics.illinois.edu>
2022-06-07 12:18:43 -0500

    Typo.

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main (int argc, char *argv[])
   eos.tbqs( 1000.0, 0.0, 0.0, 0.0, "conformal_diagonal" );
 
   bool solution_found = false;
-  double sVal = eos.s_out( 0.862529/hc, -0.308373/hc, 0.0/hc, 0.308373/hc, solution_found );
+  double sVal = eos.s_out( 0.862529, -0.308373/hc, 0.0/hc, 0.308373/hc, solution_found );
 
   if ( solution_found )
 	{
