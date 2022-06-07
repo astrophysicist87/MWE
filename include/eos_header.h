083431b083d6037b4a3de5fea1a628aff3b5a814
Christopher Plumberg <plumberg@jnorhos-03.physics.illinois.edu>
2022-06-07 16:02:59 -0500

    Smaller grid range?.

--- include/eos_header.h
+++ include/eos_header.h
@@ -7,6 +7,6 @@ static constexpr bool zero_unsolvable_charge_densities    = false;
 
 static constexpr int VERBOSE = 10;
 static constexpr double TINY = 1e-25;
-static constexpr double TBQS_INFINITY = 1e10;  // indefinitely large limits for EoS
+static constexpr double TBQS_INFINITY = 1e6;  // indefinitely large limits for EoS
 
 #endif
