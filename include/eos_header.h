a330101e1d9e4ebe5d10673fdcfafa3934d12aa3
Christopher Plumberg <plumberg@jnorhos-03.physics.illinois.edu>
2022-06-07 16:01:34 -0500

    More verbose.

--- include/eos_header.h
+++ include/eos_header.h
@@ -5,7 +5,7 @@ static constexpr bool use_rootfinder                      = true;
 static constexpr bool skip_failed_EoS                     = true;
 static constexpr bool zero_unsolvable_charge_densities    = false;
 
-static constexpr int VERBOSE = 3;
+static constexpr int VERBOSE = 10;
 static constexpr double TINY = 1e-25;
 static constexpr double TBQS_INFINITY = 1e10;  // indefinitely large limits for EoS
 
